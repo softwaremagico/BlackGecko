1a5b260841b54d1ec8b202515f343da043be1304
jorge <softwaremagico@gmail.com>
2016-08-27 21:00:14 +0200

    Added main function

---
+++ RedOwl/__init__.py
@@ -0,0 +1,3 @@
+# Keep version in a separate file so setup.py can import it separately.
+from .version import __version__
+
---
+++ RedOwl/__main__.py
@@ -0,0 +1,9 @@
+
+
+def main():
+    """Main entry point."""
+    print("RedOwl starting....")
+    
+#Execute as standard main like C++
+if __name__ == '__main__':
+    main()
