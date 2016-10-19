85fceb89b96cd5cc4cecade994c59ed0a9d8865a
Jorge Hortelano <jorgehortelano@gmail.com>
2016-10-19 21:25:54 +0200

    Implemented stackoverflow advised

--- RedOwl/sensors/sensors.py
+++ RedOwl/sensors/sensors.py
@@ -5,6 +5,7 @@ from config import ConfigurationReader
 
 class SensorsController():
 	_events_initialized = False
+	_loop = None
 
 	
 	def __init__(self, message_manager_f):
@@ -21,16 +22,40 @@ class SensorsController():
 				gpio.setmode(gpio.BCM)
 				if int(ConfigurationReader._infrared_sensor_pin) > 0 :
 					gpio.setup(ConfigurationReader._infrared_sensor_pin, gpio.IN)
-					gpio.add_event_detect(ConfigurationReader._infrared_sensor_pin, gpio.RISING, self.motion_sensor, bouncetime=500)
+					gpio.add_event_detect(ConfigurationReader._infrared_sensor_pin, gpio.RISING, callback=self.motion_detection_event, bouncetime=500)
 				if ConfigurationReader._sound_sensor_pin > 0 :
 					gpio.setup(ConfigurationReader._sound_sensor_pin, gpio.IN)
-					gpio.add_event_detect(ConfigurationReader._sound_sensor_pin, gpio.RISING, self.sound_sensor, bouncetime=500)
+					gpio.add_event_detect(ConfigurationReader._sound_sensor_pin, gpio.RISING, callback=self.sound_detection_event, bouncetime=500)
 				logging.info("Sensors enabled!")
 				asyncio.async(self._message_manager("Started..."))
+				# run the event loop
+				self._loop = asyncio.get_event_loop()
+				self._loop.run_forever()
+				self._loop.close()
 		except ImportError:
 			logging.error("No GPIO library found! Sensors are not enabled!")
 			self.sensor_error(message_manager_f, "🚫 No GPIO library found! Sensors are not enabled! 🚫")
-	
+
+
+	def motion_detection_event(self, channel):
+		print("Motion")
+		if self._loop is None:
+			print(":(")
+			return
+		# this enqueues a call to message_manager_f() 
+		self._loop.call_soon_threadsafe(self._message_manager("🚨Motion detected in '" + ConfigurationReader._alias + "'!  "))
+		print("---")
+
+
+	def sound_detection_event(self, channel):
+		print("Sound!")
+		if self._loop is None:
+			print(":(")
+			return
+		# this enqueues a call to message_manager_f() 
+		self._loop.call_soon_threadsafe(self._message_manager("🚨Motion detected in '" + ConfigurationReader._alias+  "'!  "))
+		print("---")
+
 			
 	def motion_sensor(self):
 		print("Motion")
