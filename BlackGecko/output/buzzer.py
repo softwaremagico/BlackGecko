4aa09ab29bb67d846704624a1108589069321002
Jorge Hortelano <jorgehortelano@gmail.com>
2016-11-06 11:17:38 +0100

    code cleaned up

--- BlackGecko/output/buzzer.py
+++ BlackGecko/output/buzzer.py
@@ -17,18 +17,16 @@ class Buzzer():
 			import RPi.GPIO as GPIO
 			logging.info("Alarm started!")
 			GPIO.setmode(GPIO.BCM)
+			GPIO.setwarnings(False)
 			GPIO.setup(ConfigurationReader._buzzer_pin, GPIO.OUT)
 			iteration = 0
 			sound = True
 			while iteration < 10:
 			        GPIO.output(ConfigurationReader._buzzer_pin, sound) 
-	        		pwm = GPIO.PWM(ConfigurationReader._buzzer_pin, 1)
 		        	time.sleep(1)
 			        sound = not sound
 			        iteration = iteration + 1
 
-       				#pwm.start(100 - iteration * 10)
-			pwm.stop()
 			#GPIO.cleanup()
 			logging.info("Alarm stopped!")
 		except ImportError:
